95236577f5ae2fbe8f4e9cc8e8d3921233d37916
Tamertotos <tamercantotos@gmail.com>
2026-05-27 15:11:30 +0300

    yet another assignment

---
+++ ASSIGNMENT-03/TASK1.py
@@ -0,0 +1,23 @@
+WIDTH = 40
+shopping_list = dict()
+
+while len(shopping_list) < 4:
+
+    name = input("Please enter an item name\n")
+
+    try:
+        price = int(input("Please enter its price\n"))
+    except ValueError:
+        print("Enter a numerical value")
+        continue
+
+    shopping_list[name] = price
+
+
+average = sum(shopping_list.values())/len(shopping_list)
+print("*"*WIDTH)
+print(f"{'RECEIPT':^{WIDTH}}")
+print("*"*WIDTH)
+print(f"{'Total value:':<{WIDTH-10}}{sum(shopping_list.values()):>10}")
+print(f"{'Most expensive:':<{WIDTH-10}}{max(shopping_list, key=shopping_list.get):>10}")
+print(f"{'The average price:':<{WIDTH-10}}{average:>{10}.2f}")
---
+++ ASSIGNMENT-03/TASK2.py
@@ -0,0 +1,37 @@
+import keyword
+user_names = set()
+
+while len(user_names) < 5:
+    name = input("Enter a user to be added")
+
+    if name in user_names:
+        print("This person already exists in the set")
+        continue
+
+    if name.isalpha() and not keyword.iskeyword(name):
+        user_names.add(name)
+    else:
+        print("OI")
+        continue
+
+my_list = list(user_names)
+my_list.sort()
+print(f"After sorting the set {my_list} ")
+print(f"Number of unique users is: {len(user_names)}")
+
+t = tuple(my_list[i] for i in range(3) )
+print(t)
+
+print(f"The longest name in the list: {max(my_list, key=len)}")
+
+user1 = input("Enter first user to check: ")
+user2 = input("Enter second user to check: ")
+
+if user1 in my_list and user2 in my_list:
+    print(f"{user1} and {user2} in the list")
+else:
+    print("Either or neither of them exists in the list")
+
+portion = my_list[1:3]
+print(portion)
+print(f"The longest name in the portion: {max(portion, key=len)}")
