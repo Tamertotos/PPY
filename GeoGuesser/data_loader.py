7d51251c0bf307f11c1216d7677ded1503f7cb01
Tamertotos <tamercantotos@gmail.com>
2026-07-22 21:28:32 +0300

    modifications in modules

--- GeoGuesser/data_loader.py
+++ GeoGuesser/data_loader.py
@@ -2,11 +2,17 @@ import pandas
 
 def arrange_data(path):
     data = pandas.read_csv(path)
-    data["country"] = data.country.str.lower()
     return data
 
 def get_coordinates(data,guess):
-    country_x = data[data["country"] == guess].x.iloc[0]
-    country_y = data[data["country"] == guess].y.iloc[0]
+    country_x = data[data["country"] == guess].x.item()
+    country_y = data[data["country"] == guess].y.item()
     return country_x,country_y
 
+def create_csv(data,guessed_countries):
+    countries_to_learn = [country for country in data.country if country not in guessed_countries]
+    if len(countries_to_learn) < data.shape[0]:
+        df = pandas.DataFrame(countries_to_learn)
+        df.to_csv("missed_ones.csv")
+    else:
+        print("You guessed every country correctly!")
--- GeoGuesser/main.py
+++ GeoGuesser/main.py
@@ -11,20 +11,24 @@ def setup_screen():
 
 def game_loop(screen,data):
     countries = data.country.to_list()
-    score = 0
-    while True:
-        user_answer = screen.textinput(title=f" {score}/{data.shape[0]} Guess the country", prompt="What's another country's name?").lower()
-        if user_answer in countries:
+    guessed_countries = []
+    while len(guessed_countries) < data.shape[0]:
+        user_answer = screen.textinput(title=f" {len(guessed_countries)}/{data.shape[0]} Guess the country", prompt="What's another country's name?").title()
+        if user_answer == "Exit":
+            break
+        if user_answer in countries and user_answer not in guessed_countries:
             x,y = data_loader.get_coordinates(data,user_answer)
             country = Country()
             country.move(country=user_answer,x=x,y=y)
-            score += 1
+            guessed_countries.append(user_answer)
+
+    return guessed_countries
 
 def main():
     screen = setup_screen()
     data = data_loader.arrange_data("europe_countries.csv")
-    game_loop(screen,data)
-    screen.exitonclick()
+    missed_countries = game_loop(screen,data)
+    data_loader.create_csv(data,missed_countries)
 
 if __name__ == "__main__":
     main()
