b01554a28b1a0f0dc7eb895df36d55506f4f292f
Tamertotos <tamercantotos@gmail.com>
2026-08-09 14:32:48 +0300

    added exception handling

--- Nato_Alphabet/main.py
+++ Nato_Alphabet/main.py
@@ -3,7 +3,13 @@ import pandas
 df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_alphabet = {row.letter:row.code for (index, row) in df.iterrows()}
 
-text = input("Enter a word: ").upper()
-nato_alphabet_transition = [nato_alphabet[letter] for letter in text]
 
-print(nato_alphabet_transition)
+while True:
+    text = input("Enter a word: ").upper()
+    try:
+        nato_alphabet_transition = [nato_alphabet[letter] for letter in text]
+    except KeyError as error_message:
+        print(f"Given {error_message} is not a valid key. Enter a word using Latin alphabet")
+    else:
+        print(nato_alphabet_transition)
+        break
