7ce8420e548319e5bc77ed8c4cbff9367e9a0b64
Tamertotos <tamercantotos@gmail.com>
2026-08-09 15:33:51 +0300

    added searching the file functionality

--- PasswordGeneratorGUI_App/app.py
+++ PasswordGeneratorGUI_App/app.py
@@ -28,14 +28,15 @@ class App(tkinter.Tk):
         self.label2 = tkinter.Label(self,text="Email/Username:", font=("Ariel",12,"bold"))
         self.label2.place(x=100,y=350)
 
-        self.label3 = tkinter.Label(self, text="Password", font=("Ariel", 12, "bold"))
+        self.label3 = tkinter.Label(self, text="Password:", font=("Ariel", 12, "bold"))
         self.label3.place(x=130, y=400)
 
     def build_entries(self):
-        self.entry1 = tkinter.Entry(self,width=40)
+        self.entry1 = tkinter.Entry(self,width=30)
         self.entry1.place(x=250,y=300)
+        self.entry1.focus()
 
-        self.entry2 = tkinter.Entry(self, width=40)
+        self.entry2 = tkinter.Entry(self, width=45)
         self.entry2.place(x=250, y=350)
 
         self.entry3 = tkinter.Entry(self, width=25,show="*")
@@ -43,16 +44,23 @@ class App(tkinter.Tk):
 
     def build_buttons(self,logic):
         self.button1 = tkinter.Button(self,text="Generate Password",command=logic.generate_pass)
-        self.button1.place(x=410,y=395)
+        self.button1.place(x=420,y=395)
 
         self.button2 = tkinter.Button(self,text="Add",width=40, command=logic.add)
         self.button2.place(x=245,y = 435)
 
-    def show_error(self):
-        mb.showerror("Ooops","Please don't leave any files empty!")
+        self.button3 = tkinter.Button(self, text="Search",width=11, command=logic.search)
+        self.button3.place(x=440 ,y=295)
 
-    def dialog_answer(self) -> bool:
-        if mb.askyesno(f"{self.entry1.get()}",f"Email: {self.entry2.get()}\nPassword: {self.entry3.get()}\nIs this okay?"):
+    def show_error(self,text):
+        mb.showerror(title="Oops",message=text)
+
+    def show_user_pass(self,username,password):
+        mb.showinfo(title=f"{self.entry1.get()}",message=f"Email: {username}\nPassword: {password}")
+
+    def dialog_answer(self) -> bool|None:
+        if mb.askyesno(title=f"{self.entry1.get()}",message=f"These are the details entered:\nEmail: {self.entry2.get()}\nPassword: {self.entry3.get()}\nIs it okay to save?"):
+
             return True
         else:
             mb.showinfo("Cancel","File creation is cancelled")
@@ -68,12 +76,41 @@ class Logic:
             password = self.password_manager.entry3.get()
 
             if self.password_manager.dialog_answer():
-                with open("C:\\Users\\savac\\OneDrive\\Masaüstü\\my_file.txt", "a") as f:
-                    f.write(f"{website} | {user} | {password}\n")
+                self.password_manager.entry3.delete(0, tkinter.END)
+                self.password_manager.entry1.delete(0, tkinter.END)
+                try:
+                    with open("C:\\Users\\savac\\OneDrive\\Masaüstü\\my_file.txt", "a") as f:
+                        f.write(f"{website} | {user} | {password}\n")
+                except FileNotFoundError:
+                    print("Given path could not be found!")
+                else:
+                    print("Success")
+
 
         else:
-            self.password_manager.show_error()
+            self.password_manager.show_error("Please don't leave the required entry/entries empty!")
 
+    def search(self):
+        if self.check_first_entry():
+            try:
+                with open("C:\\Users\\savacc\\OneDrive\\Masaüstü\\my_file.txt") as f:
+                    website = self.password_manager.entry1.get()
+                    file_lines = f.readlines()
+                    for line in file_lines:
+                        parts = [p.strip() for p in line.split('|')]
+                        if website in parts:
+                            self.password_manager.show_user_pass(parts[1],parts[2])
+                            break
+                    else:
+                        self.password_manager.show_user_pass("NAN","NAN")
+            except FileNotFoundError:
+                self.password_manager.show_error("You have to add before searching/Check the file path")
+            else:
+                print("Success")
+
+
+    def check_first_entry(self) -> bool:
+        return bool(self.password_manager.entry1.get())
 
     def check_entries(self) -> bool:
         return bool(self.password_manager.entry1.get() and self.password_manager.entry2.get() and self.password_manager.entry3.get())
