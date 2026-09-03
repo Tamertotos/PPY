7bbd00c0368cd9f01db742ce348cc529945e7b18
Tamertotos <tamercantotos@gmail.com>
2026-09-03 17:37:10 +0300

    creation of Question class with init and str to represent read data as question objects

--- GUIQuizzApp/main.py
+++ GUIQuizzApp/main.py
@@ -1,3 +1,7 @@
 import data
+from question_model import Question
 
-data.get_questions_api("https://opentdb.com/api.php")
+question_dictionary = data.get_questions_api("https://opentdb.com/api.php")
+question_bank = [Question(question_dictionary[i]["question"], question_dictionary[i]["correct_answer"])  for i in range(len(question_dictionary))]
+
+print(question_bank[0])
